c54a7db4b718e86b7309dc567b2642eca150ddda
MrF0o <fathihelmi36@gmail.com>
2023-04-13 21:33:19 +0100

    update .gitignore

--- routes.php
+++ routes.php
@@ -1,11 +1,15 @@
 <?php
 
+// this file is just for testing purposes
+
 include './vendor/autoload.php';
 
 include './src/Router.php';
 
+// this route is lovely, and it will greet any name!
 Router::get('/hello/{name}', function ($name) {
-    echo 'hello ' . $name;
+    echo '<h1>hello <u>' . $name . '</u></h1>';
 })->name('hello');
 
+// run the router
 Router::run();
